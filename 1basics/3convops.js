d869b6078054ca0a9e7952d9d6bedb99ddbbcb83
kirtika <86429933+Kirtikagoyal@users.noreply.github.com>
2024-08-13 12:41:21 +0000

    Convofdatatypes

---
+++ 1basics/3convops.js
@@ -0,0 +1,19 @@
+//let score = "Kirtika"
+
+//const {score} = request.body-ydk if the value is string or a no.
+
+//console.log(typeof(score));
+//node 1basics/3convops.jslet score =0;
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); //give value and it can be conv into no.
+//console.log(valueInNumber); //NAN- not a no. in js the 32abc might be conv into no, but when y check it y'll get to k it's NAN.
+// "32"->can be conv; "33abc"->NAN; true->1; Kirtika->NAN
+let isLoggedIn = "kirtika"    //empty string- false, value-true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 15
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
